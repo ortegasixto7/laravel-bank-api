c5628a4936c7dd1d990d28e18bb487e8cbec60d1
Sixto Ortega <ortegasixto7@gmail.com>
2023-08-01 21:48:45 -0400

    Adding laravel framework

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // \App\Models\UserModel::factory(10)->create();
+
+        // \App\Models\UserModel::factory()->create([
+        //     'name' => 'Test UserModel',
+        //     'email' => 'test@example.com',
+        // ]);
+    }
+}
